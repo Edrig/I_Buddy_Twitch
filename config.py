56c41765c57745c8ee09940758da4066245a6621
Noel Gaetan <gaetan.noel@viacesi.fr>
2020-06-02 23:42:40 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+TMI_TOKEN="TMI_TOKEN"
+CLIENT_ID="CLIENT_ID"
+BOT_NICK="BOT_NICK"
+BOT_PREFIX="!"     #The prefix that you want to use to denote a chatbot command.
+CHANNEL=["#edrig"] #This can be a list with multiple channels to connect to.
