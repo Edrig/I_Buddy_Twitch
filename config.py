ecb6fdefbcac759aa1964bd17b32e035056b66ab
Noel Gaetan <gaetan.noel@viacesi.fr>
2020-06-03 01:52:06 +0200

    fix .gitignore

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
-#twitch
 TMI_TOKEN="TMI_TOKEN"
 CLIENT_ID="CLIENT_ID"
 BOT_NICK="BOT_NICK"
 BOT_PREFIX="!"     #The prefix that you want to use to denote a chatbot command.
-CHANNEL=["#CHANNEL"] #This can be a list with multiple channels to connect to.
-
-#ibuddy
-productid = '0001'
-reset_position = "yes"
-
+CHANNEL=["#edrig"] #This can be a list with multiple channels to connect to.
