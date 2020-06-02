ef71f7d6d5db18dc4c3432ea046d43e78c064f5a
Noel Gaetan <gaetan.noel@viacesi.fr>
2020-06-02 23:59:23 +0200

    delete py3buddy.config

--- main.py
+++ main.py
@@ -4,59 +4,21 @@ import argparse
 import configparser
 import random
 import time
+
+import config
 import py3buddy
 
 import twitch_bot
 import globals
-ibuddy_instance = ""
 
-def init_buddy(argv):
-    parser = argparse.ArgumentParser()
+def init_buddy():
 
-    # options for the commandline
-    parser.add_argument("-c", "--config", action="store", dest="cfg",
-                        help="path to configuration file", metavar="FILE")
-    args = parser.parse_args()
-
-    # first some sanity checks for the configuration file
-    if args.cfg is None:
-        parser.error("Configuration file missing")
-
-    if not os.path.exists(args.cfg):
-        parser.error("Configuration file does not exist")
-
-    # then parse the configuration file
-    config = configparser.ConfigParser()
-
-    configfile = open(args.cfg, 'r')
-
-    try:
-        config.readfp(configfile)
-    except Exception:
-        print("Cannot read configuration file", file=sys.stderr)
-        sys.exit(1)
-
-    buddy_config = {}
-    for section in config.sections():
-        if section == 'ibuddy':
-            try:
-                productid = int(config.get(section, 'productid'))
-                buddy_config['productid'] = productid
-            except:
-                pass
-
-            buddy_config['reset_position'] = False
-            try:
-                reset_position_val = config.get(section, 'reset_position')
-                if reset_position_val == 'yes':
-                    buddy_config['reset_position'] = True
-            except:
-                pass
+    buddy_config = {'productid': int(config.productid), 'reset_position': config.reset_position}
 
     # initialize an iBuddy and check if a device was found and is accessible
-    ibuddy_instance = py3buddy.iBuddy(buddy_config)
-    globals.ibuddy = ibuddy_instance
-    if ibuddy_instance.dev is None:
+    ibuddy = py3buddy.iBuddy(buddy_config)
+    globals.ibuddy = ibuddy
+    if ibuddy.dev is None:
         print("No iBuddy found, or iBuddy not accessible", file=sys.stderr)
         sys.exit(1)
 
@@ -103,7 +65,7 @@ def panic(ibuddy, paniccount):
 def main(argv):
 
     globals.initialize()
-    init_buddy(argv)
+    init_buddy()
 
 '''
     print("Demo 4: Executing commands\n")
